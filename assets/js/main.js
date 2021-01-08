1a1024baf7f4486af9dc6773386c5930d2dc40ad
alan-mj-lin <55764881+alan-mj-lin@users.noreply.github.com>
2021-01-07 22:06:59 -0500

    center images and get rid of weird scrolling behaviour

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,9 +64,9 @@
 				$window
 					.on('scroll._parallax', function() {
 
-						var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
+						// var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
 
-						$bg.css('transform', 'matrix(1,0,0,1,0,' + (pos * intensity) + ')');
+						// $bg.css('transform', 'matrix(1,0,0,1,0,' + (pos * intensity) + ')');
 
 					});
 
